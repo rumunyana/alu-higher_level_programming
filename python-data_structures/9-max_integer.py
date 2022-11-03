fc8c5b3a44600b2d824c6d51eee9257d62294a1b
rumunyana <r.umunyana@alustudent.com>
2022-11-03 16:31:47 -0700

    new assignment

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list is None or len(my_list) == 0:
+        return None
+    max_number = my_list[0]
+    for number in my_list:
+        if number > max_number:
+            max_number = number
+    return max_number
