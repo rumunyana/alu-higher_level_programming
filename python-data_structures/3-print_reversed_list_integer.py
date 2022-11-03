bd5c68e6bcd7740a645923c6a3a0c3b827d41b2b
rumunyana <r.umunyana@alustudent.com>
2022-11-03 16:21:21 -0700

    new assignment

---
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    if my_list is None:
+        return ''
+    else:
+        my_list.reverse()
+        for a in my_list:
+            print("{:d}".format(a))
