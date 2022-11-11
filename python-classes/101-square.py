35c195f26b454f86978ad1905468d43ae695384f
rumunyana <r.umunyana@alustudent.com>
2022-11-11 08:43:07 -0800

    new assignment

--- python-classes/101-square.py
+++ python-classes/101-square.py
@@ -8,16 +8,6 @@ class Square:
     def __init__(self, size=0, position=(0, 0)):
         """Initialize a new square.
         Args:
-#!/usr/bin/python3
-"""Define a class Square."""
-
-
-class Square:
-    """Represent a square."""
-
-    def __init__(self, size=0, position=(0, 0)):
-        """Initialize a new square.
-        Args:
             size (int): The size of the new square.
             position (int, int): The position of the new square.
         """
