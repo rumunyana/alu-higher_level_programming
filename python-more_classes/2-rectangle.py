55e76fd029e3db457f1421453a7b671ca22a213d
rumunyana <r.umunyana@alustudent.com>
2022-11-11 08:59:30 -0800

    new assignment

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""define class rectangle"""
+
+
+class Rectangle:
+
+    """class Rectangle"""
+    def __init__(self, width=0, height=0):
+        """initialize width and height"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """"retrieve width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ sets width"""
+        self.__width = value
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+    @property
+    def height(self):
+        """define height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """assign height"""
+        self.__height = value
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
+    def area(self):
+        """" prints area of the rectangle"""
+        A = self.__height * self.__width
+        return A
+
+    def perimeter(self):
+        """perimeter of rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            p = 2 * (self.__height + self.__width)
+            return p
