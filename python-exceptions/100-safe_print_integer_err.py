33077fc4eb1eeace4dd79ff970269e224af6a395
rumunyana <r.umunyana@alustudent.com>
2022-11-11 06:34:40 -0800

    new assignment

---
+++ python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_integer_err(value):
+    import sys
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception as i:
+        sys.stderr.write("Exception: {}\n".format(i))
+        return False
