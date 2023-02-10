981b43b16c82fd252f8d4366749a0187c86ce6b5
rumunyana <113432535+rumunyana@users.noreply.github.com>
2023-02-10 06:59:07 +0000

    well

--- javascript-warm_up/101-call_me_moby.js
+++ javascript-warm_up/101-call_me_moby.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const callMeMoby = function (x, theFunction) {
-    for (let i = 0; i < x; i++) {
-      theFunction();
-    }
-  };
-  module.exports = {
-    callMeMoby
-  };
+  for (let i = 0; i < x; i++) {
+    theFunction();
+  }
+};
+module.exports = {
+  callMeMoby
+};
