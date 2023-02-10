06c4ccde48a02495f85426a0b2588107d68b08b4
rumunyana <113432535+rumunyana@users.noreply.github.com>
2023-02-10 06:27:18 +0000

    let's see

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const fact = parseInt(process.argv.slice(2)[0]);
+if (isNaN(fact) === true) {
+  console.log(1);
+} else {
+  const result = Factorial(fact);
+  console.log(result);
+}
+function Factorial (num) {
+  if (num === 1) {
+    return 1;
+  } else {
+    return (num * Factorial(num - 1));
+  }
+}
