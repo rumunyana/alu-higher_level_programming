0fd97b861adda3c40faf67310bbdf842afb19d02
rumunyana <113432535+rumunyana@users.noreply.github.com>
2023-02-10 06:43:23 +0000

    go

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
 function factorial (number) {
-    if (isNaN(parseInt(number))) {
-      return (1);
+  if (isNaN(parseInt(number))) {
+    return (1);
     }
-    if (number === 1) {
+  if (number === 1) {
       return (1);
     }
     return (number * factorial(number - 1));
